983e2da981a872a3a65826a01dea627e13292937
sakrobinson <scott.austen.robinson@gmail.com>
2024-03-21 10:17:49 -0600

    multiple edits

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-app.use(express.json());
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
